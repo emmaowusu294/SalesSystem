6af86d90bf3b8f8e244dda65ca93fc4f088122de
4Ts-code <eo8407272@gmail.com>
2025-11-06 13:34:32 +0000

    minor tweaks

---
+++ ViewModels/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesSystem.ViewModels
+{
+    public class CustomerViewModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}
